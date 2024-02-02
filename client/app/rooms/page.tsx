9927402a5f8f3cc580287f434f09a754dc596ca7
Tobias Sassin <tobi.sassin@gmail.com>
2024-02-02 10:54:17 +0100

    initial

--- client/app/rooms/page.tsx
+++ client/app/rooms/page.tsx
@@ -1,27 +1,44 @@
 "use client";
 
-import React, { use, useEffect, useState } from 'react';
-import { useSearchParams } from 'next/navigation';
+import React, { use, useEffect, useState, useContext } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
 import QrCode from '@/components/QrCode';
 import { useUrl } from 'nextjs-current-url';
 import useUser from '../useUser';
 
+import { io } from "socket.io-client";
+import { validateRoomId } from '@/utils/helpers';
+import { useAuth } from '@/contexts/AuthContext'; // Import the AuthContext
+
+const socket = io("http://localhost:3001");
+
 const Page = () => {
     const searchParams = useSearchParams();
+    const page_roomId = searchParams.get("id");
+    const router = useRouter();
     const [isQROpen, setQROpen] = useState(false);
     const { href } = useUrl() ?? { href: null }; 
     const { data, isLoading } = useUser();
     console.log("heref",href);
     const QrCodeTitle = "Join Room: " + searchParams.get("id");
 
+    useEffect(() => {
+        socket.emit('joinRoom', page_roomId);
+    }, [page_roomId]);
+
+    useEffect(() => {
+        socket.on('roomDoesNotExist', () => {
+            router.replace("/join");
+        });
+    }, [router]);
     return (
         <div className='flex flex-col gap-2 h-full w-full'>
             <div className='flex bg-blue-300 h-1/5'>
-                <p className='text-center m-auto'>Room ID: {searchParams.get("id")}</p>
+                <p className='text-center m-auto'>Room ID: {page_roomId}</p>
                 <button onClick={() => setQROpen(true)}>Open QR</button>
             </div>
             <div className='flex h-full bg-gray-400'>
-                <h1 className='text-2xl m-auto'> 
+                <h1 className='text-2xl m-auto'>
                     Inhalt
                     <div>
                         {isLoading ? (
--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,8 @@ const express = require('express')
 const http = require('http')
 const app = express()
 const server = http.createServer(app)
+const port = process.env.SOCKET_IO_PORT || 3001
+console.log("env", process.env.SOCKET_IO_PORT);
 
 import { Server } from 'socket.io'
 const io = new Server(server, {
@@ -17,6 +19,6 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(3001, () => {
-  console.log('listening on *:3001')
+server.listen(port, () => {
+  console.log(`listening on *:${port}`)
 })
